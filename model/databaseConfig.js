45d9bb9b6bd3b32875e843277f957af974b771ec
Tan Guan Ying <guanying.23@ichat.sp.edu.sg>
2025-12-15 23:00:15 +0800

    fix: fix database error auth

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -4,7 +4,7 @@ var dbconnect = {
         var conn = mysql.createConnection({
             host: "localhost",
             user: "root",
-            password: "haha",
+            password: "root1234",
             database: "islandfurniture-it07"
         });
         return conn;
