c4357528f308d31dfddffdb820db1155a94d3a05
funnifox <snoowy.foox@gmail.com>
2025-12-25 19:00:24 +0800

    chore: setup for draggables

--- view/A7/js/editShowroomFurniture.js
+++ view/A7/js/editShowroomFurniture.js
@@ -17,11 +17,11 @@ function GetURLParameter(sParam){
     }
 }
 
-var countryId = localStorage.getItem('countryId');
 function getFurniture(arr){
     console.log(arr);
 
     let furnitureHTML = '';
+    let draggableFurnitureHTML = '';
     for (let i = 0; i < arr.length; i++) {
         furnitureHTML += `
         <div class="furniture-item">
@@ -30,6 +30,16 @@ function getFurniture(arr){
             <button class="btn-delete btn" style="width:100%;" data-id="${arr[i].ID}"><i class="fas fa-trash"></i> Delete</button>
         </div>
             `
+
+        draggableFurnitureHTML += `
+            <div id="mydiv">
+            <div id="mydivheader">Click here to move</div>
+            <p>Move</p>
+            <p>this</p>
+            <p>DIV</p>
+            </div>
+            `
+
     }
 
 
@@ -37,9 +47,21 @@ function getFurniture(arr){
         <div class="furniture-item add-furniture" style="background-color: transparent;">
             <button class="popup-btn"><i class="fas fa-plus-circle"></i></button>
         </div>
-`
-    document.getElementById('furniture-list').innerHTML = furnitureHTML;
+        
+    `
     
+    
+    document.getElementById('furniture-list').innerHTML = furnitureHTML;
+    const showroom = document.getElementById('showroom');
+
+    // this is to prevent the drag element thing from being unable to find the 
+    // div when it hasnt loaded yet. temp to serve as a placehodler for while draggabel div aint loaded
+    const temp = document.createElement('div');
+    temp.innerHTML = draggableFurnitureHTML.trim();
+    const newDiv = temp.firstChild;
+    showroom.appendChild(newDiv);
+    dragElement(newDiv);
+
 } 
 
 /// no you cannot add a kit kat to the showroom.
@@ -84,17 +106,49 @@ function del(id){
         console.log(error);
     });
 }
-// get single item by sku (country id is not needed but nice to have)
-// fetch(`/api/getFurnitureBySku?sku=${arr[i].SKU}&countryId=${countryId}`, {
-//     method: 'GET',
-// }).then(function (response) {
-//     return response.json();
-// }).then(function (product) {
-//     console.log(product);
-// }).catch(function(error) {
-//     console.log(error);
-// });
 
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_draggable
+//Make the DIV element draggagle:
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
 
 
 function openPopup() {document.getElementById("popup").style.display = "block";}
@@ -107,7 +161,16 @@ window.onclick = function(event) {
     }
 };
 
-
+// get single item by sku (country id is not needed but nice to have)
+// fetch(`/api/getFurnitureBySku?sku=${arr[i].SKU}&countryId=${countryId}`, {
+//     method: 'GET',
+// }).then(function (response) {
+//     return response.json();
+// }).then(function (product) {
+//     console.log(product);
+// }).catch(function(error) {
+//     console.log(error);
+// });
 
 
 document.addEventListener('DOMContentLoaded', function () {
