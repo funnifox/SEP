4c29410ecb71c913421c7b53e597221a460873ed
Tan Guan Ying <guanying.23@ichat.sp.edu.sg>
2026-01-28 00:07:36 +0800

    feat: added playwright test for entering information to update and submitting

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
+/* These test to open the official playwright website, it is not required for our testing
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
@@ -17,3 +18,56 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+*/
+
+//---------------------------------------
+// Tests developed by Guan Ying
+// login
+// npx playwright test --project=webkit --headed
+test('login', async ({ page }) => {
+  await page.goto('http://localhost:8081/B/selectCountry.html');
+  await page.locator('a:has-text("Singapore")').click();
+  await page.locator('a:has-text("Login/Register")').click();
+
+  // login
+  await page.fill("#emailLogin", "dramatictan69@gmail.com");
+  await page.fill("#passwordLogin", "12345678");
+  await page.click('input[type="submit"]');
+})
+
+// Edit user information (e.g. Name, Phone, Address, Country, Security Question, Age, Income without changing password)
+test('edit personal information', async ({ page }) => {
+  await page.goto('http://localhost:8081/B/selectCountry.html');
+  await page.locator('a:has-text("Singapore")').click();
+  await page.locator('a:has-text("Login/Register")').click();
+
+  // login
+  await page.fill("#emailLogin", "dramatictan69@gmail.com");
+  await page.fill("#passwordLogin", "12345678");
+  await page.click('input[type="submit"]');
+
+  // fill in fields
+  await page.fill("#name", "Tan Guan Ying");
+  await page.fill("#phone", "91234567");
+  await page.selectOption("#country", "Singapore");
+  await page.fill("#address", "Blk 123");
+  await page.fill("#securityAnswer", "Mom");
+  await page.fill("#age", '20');
+  await page.fill("#income", "1200");
+
+  const slaCheckbox = page.locator("#serviceLevelAgreement");
+  if (!(await slaCheckbox.isChecked())) {
+    await slaCheckbox.check();
+  }
+
+  // click submit button
+  await page.click('input[type="submit"]');
+
+  // detect Fields updated and a status stating, “Successfully Updated!”
+  await expect(page.locator('#goodDiv')).toHaveText('Successfully Updated!');
+})
+
+// enter password 
+test('update password', async ({ page }) => {
+  
+})
