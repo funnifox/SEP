b169e5bcddba69882dd2a0017f08fd0b894c1beb
Tan Guan Ying <guanying.23@ichat.sp.edu.sg>
2026-01-28 20:58:35 +0800

    feat: finish playwright test
    - test is able to complete all 12 steps based on my test case steps
    
    - added error handling checks of all possible password input errors
    
    - added code to reset password to that this test can be repeated anytime

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -24,28 +24,19 @@ test('get started link', async ({ page }) => {
 // Tests developed by Guan Ying
 // login
 // npx playwright test --project=webkit --headed
-test('login', async ({ page }) => {
+
+// Test the test case all at one go
+test('automated test for editing user profile', async ({ page }) => {
   await page.goto('http://localhost:8081/B/selectCountry.html');
   await page.locator('a:has-text("Singapore")').click();
   await page.locator('a:has-text("Login/Register")').click();
 
-  // login
-  await page.fill("#emailLogin", "dramatictan69@gmail.com");
-  await page.fill("#passwordLogin", "12345678");
-  await page.click('input[type="submit"]');
-})
-
-// Edit user information (e.g. Name, Phone, Address, Country, Security Question, Age, Income without changing password)
-test('edit personal information', async ({ page }) => {
-  await page.goto('http://localhost:8081/B/selectCountry.html');
-  await page.locator('a:has-text("Singapore")').click();
-  await page.locator('a:has-text("Login/Register")').click();
-
-  // login
+  // ----------------- Step 1: Open Member Profile Page -----------------
   await page.fill("#emailLogin", "dramatictan69@gmail.com");
   await page.fill("#passwordLogin", "12345678");
   await page.click('input[type="submit"]');
 
+  // ----------------- Step 2: Edit user info -----------------
   // fill in fields
   await page.fill("#name", "Tan Guan Ying");
   await page.fill("#phone", "91234567");
@@ -55,74 +46,80 @@ test('edit personal information', async ({ page }) => {
   await page.fill("#age", '20');
   await page.fill("#income", "1200");
 
+  // Step 3: Ignored the password fields
+
   const slaCheckbox = page.locator("#serviceLevelAgreement");
   if (!(await slaCheckbox.isChecked())) {
     await slaCheckbox.check();
   }
 
-  // click submit button
+  // ----------------- Step 4: Click "submit" button -----------------
   await page.click('input[type="submit"]');
 
-  // detect Fields updated and a status stating, “Successfully Updated!”
+  // Expected Response: fields updated and a status stating, “Successfully Updated!”
   await expect(page.locator('#goodDiv')).toHaveText('Successfully Updated!');
-})
 
-// enter password and update it, then log out and relogin with new password
-test('update password', async ({ page }) => {
-  await page.goto('http://localhost:8081/B/selectCountry.html');
-  await page.locator('a:has-text("Singapore")').click();
-  await page.locator('a:has-text("Login/Register")').click();
+  // ----------------- Step 5: Try updating password with empty new password -----------------
+  // fill in password fields
+  await page.fill("#oldPassword", "12345678");
+  await page.click('input[type="submit"]');
 
-  // login
+  // Expected Response: System show status with “New Password cannot be empty.” and prevents password update
+  await expect(page.locator('#errDiv')).toHaveText('New Password cannot be empty.');
+
+  // ----------------- Step 6: Try leaving old password empty -----------------
+  await page.fill("#password", "I_Love_SEP01!");
+  await page.fill("#repassword", "I_Love_SEP01!");
+  await page.click('input[type="submit"]');
+
+  // System show status with “Please enter your old password to change it.” and prevents password update
+  await expect(page.locator('#errDiv')).toHaveText('Please enter your old password to change it.');
+
+  // ----------------- Step 7: Try entering the wrong password  -----------------
+  await page.fill("#oldPassword", "WRONG_PW");
+  await page.fill("#password", "I_Love_SEP01!");
+  await page.fill("#repassword", "I_Love_SEP01!");
+  await page.click('input[type="submit"]');
+
+  // System show status with “Old password is incorrect.” and prevents password update
+  await expect(page.locator('#errDiv')).toHaveText('Old password is incorrect.');
+
+  // ----------------- Step 8: Enter current password correctly in “Old Password“ field -----------------
+  await page.fill("#oldPassword", "12345678");
+
+  // ----------------- Step 9:  Enter new password and re-enter password the desired new password -----------------
+  await page.fill("#password", "I_Love_SEP01!");
+  await page.fill("#repassword", "I_Love_SEP01!");
+
+  // ----------------- Step 10: Click "Submit" button -----------------
+  await page.click('input[type="submit"]');
+
+  // Expected Response: Fields updated and a status stating, “Successfully Updated!”
+  await expect(page.locator('#goodDiv')).toHaveText('Successfully Updated!');
+
+  // ----------------- Step 11: Attempt to log in with old password -----------------
+  await page.locator('a:has-text("Logout")').click(); // logout first
   await page.fill("#emailLogin", "dramatictan69@gmail.com");
   await page.fill("#passwordLogin", "12345678");
   await page.click('input[type="submit"]');
 
-  /** REFERNECE HTML CODE
-    <div class="form-group">
-        <label>Old Password (leave blank unless setting a new password).</label>
-        <input class="form-control" type="password" id="oldPassword">
-    </div>
-    <div class="form-group">
-        <label>New Password<br/>Password to be at least 8 characters long.</label>
-        <input class="form-control" type="password" id="password">
-    </div>
-    <div class="form-group">
-        <label>Re-enter New Password</label>
-        <input class="form-control" type="password" id="repassword">
-    </div>
-   */
+  // Expected Response: Login fails with a status message of “Login fail. Email or password is incorrect.” 
+  await expect(page.locator('#errDiv')).toHaveText('Login fail. Email or password is incorrect.');
 
-  // fill in password fields
-  await page.fill("#oldPassword", "12345678");
-  await page.fill("#password", "I_Love_SEP01!");
-  await page.fill("#repassword", "I_Love_SEP01!");
-
-  // click submit button
-  await page.click('input[type="submit"]');
-
-  // detect Fields updated and a status stating, “Successfully Updated!”
-  await expect(page.locator('#goodDiv')).toHaveText('Successfully Updated!');
-
-  // logout and login with new password
-  await page.locator('a:has-text("Logout")').click();
-  await page.fill("#emailLogin", "dramatictan69@gmail.com");
-  await page.fill("#passwordLogin", "I_Love_SEP01!");
-  await page.click('input[type="submit"]');
-})
-
-// enter password and update it, then log out and relogin with new password
-// npx playwright test -g 'reset test'
-test('reset test', async ({ page }) => {
-  await page.goto('http://localhost:8081/B/selectCountry.html');
-  await page.locator('a:has-text("Singapore")').click();
-  await page.locator('a:has-text("Login/Register")').click();
-
-  // login
+  // ---------------- Step 12: Log out and log in using new password ----------------
   await page.fill("#emailLogin", "dramatictan69@gmail.com");
   await page.fill("#passwordLogin", "I_Love_SEP01!");
   await page.click('input[type="submit"]');
 
+  // Expected Response: Login succeeds using new password and automatically redirect to http://localhost:8081/B/SG/memberProfile.html
+  await expect(page).toHaveURL('http://localhost:8081/B/SG/memberProfile.html');
+
+  // Post-conditions: 
+  // -	User profile particulars such as Name, Phone, Address, Challenge, Country, Security Question, Age and Income are updated in database
+  // -	User password is updated in database if changed
+
+
+  //-------------------------------- RESET TEST PASSWORD INFO ---------------------------------
   // fill in password fields
   await page.fill("#oldPassword", "I_Love_SEP01!");
   await page.fill("#password", "12345678");
